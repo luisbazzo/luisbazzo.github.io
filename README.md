3c62aec6a2276ee9d574c29d9fd07ec5dcf76a73
luisbazzo <luis.o.c.bazzo@gmail.com>
2023-05-20 11:58:25 -0300

    1/4 done

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio Website 
+
+Link to the page: https://luisbazzo.github.io
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
